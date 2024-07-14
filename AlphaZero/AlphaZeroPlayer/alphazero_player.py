6358082bfa29a9fcf78b9580547ff96bbcbd0b7b
RJHazelaar <robert-jan-hazelaar@hotmail.com>
2024-07-14 02:30:10 +0200

    try without nat in training

--- AlphaZero/AlphaZeroPlayer/alphazero_player.py
+++ AlphaZero/AlphaZeroPlayer/alphazero_player.py
@@ -28,6 +28,9 @@ class AlphaZero_player:
     def update_state(self, move: Card):
         self.state.do_move(move)
 
+    def update_state_alt(self, move: Card):
+        self.state.do_move_alt(move)
+
     def get_move(self, training: bool = False, extra_noise_ratio=0):
         if self.player_position != self.state.current_player:
             print(self.state.current_player, self.player_position)
