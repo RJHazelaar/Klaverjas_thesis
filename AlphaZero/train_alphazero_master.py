3481e4947312c28bd9ad3669662d54e61fec712e
RJHazelaar <robert-jan-hazelaar@hotmail.com>
2024-08-15 02:30:32 +0200

    Update train_alphazero_master.py

--- AlphaZero/train_alphazero_master.py
+++ AlphaZero/train_alphazero_master.py
@@ -333,7 +333,7 @@ def train(
 
         nn_scaler_frequency = 30
         if step % nn_scaler_frequency == 0 and mcts_params["mcts_params"]["nn_scaler"] < 1:
-            mcts_params["mcts_params"]["nn_scaler"] += 0.2_
+            mcts_params["mcts_params"]["nn_scaler"] += 0.2
     # always test at the end
     if step % test_frequency != 0:
         scores_round, _, _ = run_test_multiprocess(
