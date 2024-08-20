04cf7488ca06f84475b9b4db5b9e6423c2615811
RJHazelaar <robert-jan-hazelaar@hotmail.com>
2024-08-21 01:40:05 +0200

    Update mcts_master.py

--- AlphaZero/AlphaZeroPlayer/mcts_master.py
+++ AlphaZero/AlphaZeroPlayer/mcts_master.py
@@ -199,6 +199,8 @@ class MCTS:
         all_cards = [0,1,2,3,4,5,6,7,10,11,12,13,14,15,16,17,20,21,22,23,24,25,26,27,30,31,32,33,34,35,36,37]
         dic = dict(zip(legal_moves_list_id, probabilities))
         policy = [0 if x not in dic else dic[x] for x in all_cards]
+        print("MCTS_Master Policy")
+        print(policy)
 
         return move, policy
 
