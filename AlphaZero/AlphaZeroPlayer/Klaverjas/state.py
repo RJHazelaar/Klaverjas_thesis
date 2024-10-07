70fe4321bc8c655fcab9f9ea03493d205f68d2f1
RJHazelaar <robert-jan-hazelaar@hotmail.com>
2024-10-07 04:47:25 +0200

    Update state.py

--- AlphaZero/AlphaZeroPlayer/Klaverjas/state.py
+++ AlphaZero/AlphaZeroPlayer/Klaverjas/state.py
@@ -108,6 +108,8 @@ class State:
         for index, player in enumerate(other_players):
             self.hands[player] = hands[index]
 
+        print("Determinization: ", self.hands)
+
     def reset_determinization(self, hands):
         all_hands = [set() for _ in range(4)]
         for player, hand in enumerate(hands):
